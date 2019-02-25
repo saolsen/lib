b9e2e5be45020703f446b31b65c6f41b6e411c21
saolsen <steve.olsen@gmail.com>
2019-02-25 09:02:58 -0500

    init

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CODE_PATH="$(dirname "$0")"
+
+mkdir -p "$CODE_PATH/build"
+pushd "$CODE_PATH/build"
+
+clang -O2 -g -std=c99 -Wall -fdiagnostics-absolute-paths \
+    -o test_steve \
+    ../test_steve.c
+
+popd
---
+++ test_steve.c
@@ -0,0 +1,7 @@
+#include "steve.h"
+
+#include <stdio.h>
+
+int main() {
+    printf("Testing Steve Lib\n");
+}
